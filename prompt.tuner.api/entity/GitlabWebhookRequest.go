1667bbb4bdb35f59d05207026b10b1934a6daf71
never934 <never_934@bk.ru>
2025-10-28 18:19:57 +0300

    Update GitlabWebhookRequest.go

--- prompt.tuner.api/entity/GitlabWebhookRequest.go
+++ prompt.tuner.api/entity/GitlabWebhookRequest.go
@@ -4,13 +4,13 @@ type GitlabWebhookRequest struct {
 	ProjectId        int                                  `json:"project_id"`
 	ObjectAttributes GitlabWebhookRequestObjectAttributes `json:"object_attributes"`
 	User             GitlabWebhookRequestUser             `json:"user"`
-	AwardedOnUrl     string                               `json:"awarded_on_url"`
 	Note             GitlabWebhookRequestNote             `json:"note"`
 }
 
 type GitlabWebhookRequestObjectAttributes struct {
-	Action string `json:"action"`
-	Name   string `json:"name"`
+	Action       string `json:"action"`
+	Name         string `json:"name"`
+	AwardedOnUrl string `json:"awarded_on_url"`
 }
 
 type GitlabWebhookRequestUser struct {
